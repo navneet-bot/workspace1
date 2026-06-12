cc91f6d23e38197469cf08f7f3ec02a04d9504b0
mani <mani@manis-MacBook-Air.local>
2026-06-13 01:12:39 +0530

    fixed

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,6 +6,8 @@ import { DashboardHeader } from "@/components/layout/DashboardHeader";
 import { MobileOverlay } from "@/components/layout/MobileOverlay";
 import { PageBody } from "@/components/layout/PageBody";
 
+export const dynamic = "force-dynamic";
+
 export default async function DashboardLayout({
   children,
 }: {
